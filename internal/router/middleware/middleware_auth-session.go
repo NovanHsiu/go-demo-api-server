22a821e8ed33db001333a185de3c18ffcb38019c
Huey Yu <huey_yu@acebiotek.com>
2024-08-14 13:15:54 +0800

    fix(router/middleware): Modify the AuthSessionToken middleware by adding caching to improve performance.

--- internal/router/middleware/middleware_auth-session.go
+++ internal/router/middleware/middleware_auth-session.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/NovanHsiu/go-demo-api-server/internal/domain/common"
 	"github.com/NovanHsiu/go-demo-api-server/internal/domain/constant"
@@ -12,7 +13,15 @@ import (
 func (m *Middleware) AuthSessionToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
-		token := session.Get("token")
+		var token interface{}
+		if cacheToken, ok := m.App.Cache.SessionTokenCache.Get(session.ID()); ok {
+			token = cacheToken
+		} else {
+			token = session.Get("token")
+			if token != nil {
+				m.App.Cache.SessionTokenCache.Add(session.ID(), token, 10*time.Minute)
+			}
+		}
 		if token == nil {
 			c.JSON(http.StatusUnauthorized, common.GetResponseObject(40101, "token undefined"))
 			c.Abort()
